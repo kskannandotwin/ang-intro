395b5b3544e691bd48af2841e03887e3620b99de
kannanATexcelforte <kannan@excelforte.com>
2023-05-24 11:32:37 +0530

    24.05.2023
    completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,17 @@
-import { Component, ViewChild, AfterViewInit, ChangeDetectorRef } from '@angular/core';
-import { PostComponent } from './post/post.component';
+import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent {
   title = 'ang-intro';
-  parentMessage: string = 'Message changed';
-  // message: string | undefined;
-  message: string = 'Message from typescript component file';
-  imgUrl: string = 'https://smaller-pictures.appspot.com/images/dreamstime_xxl_65780868_small.jpg';
-  fromChildOutput: string | undefined;
   bool: boolean = true;
 
-  @ViewChild(PostComponent) childComp: any;
-
-  // if you didn't use ChangeDetectorRef, face error in console.
-  constructor(private cdr: ChangeDetectorRef) {
-    // console.log(this.childComp);
-  }
-
-  ngAfterViewInit() {
-    // console.log(this.childComp);
-    // console.log(this.childComp.childMessage);
+  buttonClick() {
+    console.log('Button click event worked');
     
-    this.message = this.childComp.childMessage;
-    this.cdr.detectChanges();
-  }
-
-  receiveMessage($event: any) {
-    // console.log($event);
-    this.fromChildOutput = $event;    
   }
 }
