dd0b192bc55d6a45aea315e11af6f9d4e8328895
kannanATexcelforte <kannan@excelforte.com>
2023-06-05 12:47:03 +0530

    05.06.2023
    completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,28 @@
 import { Component } from '@angular/core';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  postArray: Array<string> = ['Post 1', 'Post 2', 'Post 3', 'Post 4', 'Post 5'];
-  objArray: Array<any> = [];
+  name: string | undefined;
+  email: string | undefined;
+  address: string | undefined;
 
-  stepForm: string | undefined = 'Something Else';
-  isActive: boolean = true;
+  userArray: Array<any> = [];
 
-  constructor() {
-    // for (let i = 0; i < this.postArray.length; i++) {
-    //   console.log(this.postArray[i]);      
-    // }
-  }
+  onClick() {
+    this.userArray.push({
+      'name': this.name,
+      'email': this.email,
+      'address': this.address
+    });
 
-  addNew() {
-    this.objArray.push({ id: 6, postTitle: 'Post 6'});
+    console.log(this.userArray);    
   }
 
   onDelete(index: number) {
-    this.objArray.splice(index, 1);
-  }
-
-  onClick(status: string | undefined) {
-    this.stepForm = status;
+    this.userArray.splice(index, 1);
   }
 }
