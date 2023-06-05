07f2e4f18877c7b172951077775567c6b099134b
kannanATexcelforte <kannan@excelforte.com>
2023-06-05 14:22:19 +0530

    05.06.2023
    completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,23 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name: string | undefined;
-  email: string | undefined;
-  address: string | undefined;
-
-  userArray: Array<any> = [];
-
-  onClick() {
-    this.userArray.push({
-      'name': this.name,
-      'email': this.email,
-      'address': this.address
-    });
-
-    console.log(this.userArray);    
-  }
-
-  onDelete(index: number) {
-    this.userArray.splice(index, 1);
-  }
+  title: string = 'Angular Course';
 }
