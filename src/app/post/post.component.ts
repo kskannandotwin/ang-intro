e0a1439efca6006412c6ce669a464e2a586bcb83
kannanATexcelforte <kannan@excelforte.com>
2023-06-06 16:32:15 +0530

    06.06.2023
    completed

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PostService } from '../services/post.service';
+import { Post } from '../models/post';
 
 @Component({
   selector: 'app-post',
@@ -16,7 +17,7 @@ export class PostComponent {
   }
 
   addNewData() {
-    let newPost = {
+    let newPost: Post = {
       id: 7,
       postTitle: 'Post 7'
     }
--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -19,5 +19,6 @@ export class PostService {
 
     addPost(data: any) {
         this.postList.push(data);
+        console.log(this.postList);        
     }
 }
