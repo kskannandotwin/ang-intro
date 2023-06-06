a6284e8fca231ee5b0f8edb0511f87f2a1328c6d
kannanATexcelforte <kannan@excelforte.com>
2023-06-07 04:56:39 +0530

    07.06.2023
    completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,32 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title: string = 'Angular Course';
-  count: number = 285645;
-  dcValue: number = 3.85674;
-  price: number = 99.99;
-
-  today: Date = new Date();
-
-  postObj: object = {
-    id: 1,
-    postTitle: 'Post 1'
-  }
-
-  postArray: Array<string> = [
-    'post 1',
-    'post 2',
-    'post 3',
-    'post 4',
-    'post 5',
-    'post 6'
-  ];
-
-  userdetails = {
-    name: 'User 1',
-    city: 'Newyork',
-    countryCode: 'US'
-  }
-
-  dummyText: string = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam suscipit eum aperiam quisquam adipisci unde vitae cupiditate totam ipsam iure inventore quaerat expedita sit sunt reiciendis quos, ea autem sint.'
+  onSubmit(f: NgForm) {
+    console.log(f);    
+  }  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PostComponent } from './post/post.component';
 import { PostListComponent } from './post-list/post-list.component';
-import { FormsModule } from '@angular/forms';
+
 import { AppendPipe } from './pipes/append.pipe';
 import { AppendCLIPipe } from './pipes/append-cli.pipe';
 import { SummaryPipe } from './pipes/summary.pipe';
+
 import { PostService } from './services/post.service';
 @NgModule({
   declarations: [
@@ -23,8 +25,9 @@ import { PostService } from './services/post.service';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
+
   ],
   providers: [
   ],
