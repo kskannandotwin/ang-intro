6991baef60bcbe8fd776e761e7f58113a30c6ade
kannanATexcelforte <kannan@excelforte.com>
2023-06-12 14:55:54 +0530

    12.06.2023
    completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, NgForm, NgModel } from '@angular/forms';
+import { NgForm, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
