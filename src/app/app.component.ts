f0bc6c088f548f809e6d9dfe7dd34b3cfcc2d8c0
kannanATexcelforte <kannan@excelforte.com>
2023-06-20 13:01:47 +0530

    20.06.2023
    completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormArray, FormControl, FormGroup, NgForm, NgModel, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, NgForm, NgModel, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -12,27 +12,50 @@ export class AppComponent {
   emailRegex: string = '[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$';
   contactRegex: string = '[789][0-9]{9}';
 
-  constructor() {
-    this.form = new FormGroup({
-      fullName: new FormControl('', [
+  constructor(fb: FormBuilder) {
+    this.form = fb.group({
+      fullName: ['', [
         Validators.required,
         Validators.minLength(5)
-      ]),
-      email: new FormControl('', [
+      ]],
+
+      email: ['', [
         Validators.required,
-        // Validators.pattern(this.emailRegex)
         Validators.email
-      ]),
-      contactDetails: new FormGroup({
-        address: new FormControl('', Validators.required),
-        shippingAddress: new FormControl('', Validators.required),
-        contactNo: new FormControl('', [
+      ]],
+
+      contactDetails: fb.group({
+        address: ['', Validators.required],
+        shippingAddress: ['', Validators.required],
+        contactNo: ['', [
           Validators.required,
           Validators.pattern(this.contactRegex)
-        ])
+        ]]
       }),
-      skills: new FormArray([])
-    });
+
+      skills: fb.array([])
+    })
+
+    // this.form = new FormGroup({
+    //   fullName: new FormControl('', [
+    //     Validators.required,
+    //     Validators.minLength(5)
+    //   ]),
+    //   email: new FormControl('', [
+    //     Validators.required,
+    //     // Validators.pattern(this.emailRegex)
+    //     Validators.email
+    //   ]),
+    //   contactDetails: new FormGroup({
+    //     address: new FormControl('', Validators.required),
+    //     shippingAddress: new FormControl('', Validators.required),
+    //     contactNo: new FormControl('', [
+    //       Validators.required,
+    //       Validators.pattern(this.contactRegex)
+    //     ])
+    //   }),
+    //   skills: new FormArray([])
+    // });
   }
 
   get Fullname() {
